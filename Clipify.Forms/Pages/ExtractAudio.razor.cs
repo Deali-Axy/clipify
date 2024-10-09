d0189e903d7442899898fbbb6ff08d51789141fd
DealiAxy <dealiaxy@gmail.com>
2024-10-09 22:10:57 +0800

    🔧修复新的ffmpeg参数导致提取音频失败的问题

--- Clipify.Forms/Pages/ExtractAudio.razor.cs
+++ Clipify.Forms/Pages/ExtractAudio.razor.cs
@@ -45,7 +45,7 @@ public partial class ExtractAudio {
             return null;
         }
 
-        return $"-y -hide_banner -progress pipe:1 -i \"{VideoPath}\" -map a -c:a \"{OutputPath}\"";
+        return $"-y -hide_banner -progress pipe:1 -i \"{VideoPath}\" -map a -c:a copy \"{OutputPath}\"";
     }
 
     private async Task OpenFileDialog() {
